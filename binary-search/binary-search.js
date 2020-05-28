e5232410a2606edba3f3fa62c3d2c80af0cf5069
edeniz9191@gmail.com <edeniz9191@gmail.com>
2020-05-28 15:38:56 -0400

    binary search completed

--- binary-search/binary-search.js
+++ binary-search/binary-search.js
@@ -2,7 +2,25 @@
 
 // Complete this algo
 const binarySearch = (array, target) => {
-	
+	//find the midpoint of the array
+	let middle = Math.floor(array.length/2)
+	//divide array into left and right sub arrays
+	let leftArray = array.slice(0, middle)
+	let rightArray = array.slice(middle, array.length)
+
+	//if the array only has one element and it's not equal to target, return false
+	if (array.length ===1 && array[0] !== target) {
+		return false
+	} 
+
+	if (array[middle]===target) {
+		return true
+	} else if (array[middle]<target) {
+		return binarySearch(rightArray, target)
+	} else if (array[middle]>target) {
+		return binarySearch(leftArray, target)
+	}
+	return false
 };
 
 /*
--- binary-search/binary-search.spec.js
+++ binary-search/binary-search.spec.js
@@ -5,6 +5,7 @@ const binarySearch = require('./binary-search');
 const test1 = [1,3,4,5,10,15,20,22,23,98,1000];
 const test2 = [-3,-2,0, 9, 10, 11, 12, 15, 17, 18, 19, 20];
 const test3 = [-0.5, 0.03, 2.6, 3, 3.5, 3.777777, 5];
+const test4 = [5]
 
 
 describe('Binary Search', () => {
@@ -13,11 +14,13 @@ describe('Binary Search', () => {
 		expect(binarySearch(test2, -3)).to.equal(true);
 		expect(binarySearch(test2, 20)).to.equal(true);
 		expect(binarySearch(test3, 2.6)).to.equal(true);
+		expect(binarySearch(test4, 5)).to.equal(true);
 	});
 
 	it('returns false if target value is not in array', () => {
 		expect(binarySearch(test2, 25)).to.equal(false);
 		expect(binarySearch(test3, 4)).to.equal(false);
+		expect(binarySearch(test4, 4)).to.equal(false)
 	});
 
 	//(OPTIONAL) WRITE YOUR OWN TESTS HERE. Include >=1 edge case :)
