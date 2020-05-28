7641c90fd5dc146f07518a2342cb0260f815a6c2
Veronica Minciuna <veronicaminci@gmail.com>
2020-05-28 17:51:34 -0400

    work in progress

--- min-jumps/min-jumps.js
+++ min-jumps/min-jumps.js
@@ -1,8 +1,16 @@
-'use strict';
+"use strict";
 
 // Complete this algo
-const minJumps = arr => {
+const minJumps = (arr) => {
+  let counter = 0;
+  let index = 0;
+  if (index < arr.length - 1) {
+    let maxJumps = arr[index];
 
+    let numberofJumps = arr.slice(index, maxJumps);
+    counter++;
+    console.log(numberofJumps);
+  }
 };
 
-module.exports = minJumps
+module.exports = minJumps;
